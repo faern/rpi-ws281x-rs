f276ffd71302c885aab58b11025d807cf4cc404b
Linus Färnstrand <faern@faern.net>
2020-06-11 22:44:25 +0200

    Allow specifying C library lib dir as environment variable

--- build.rs
+++ build.rs
@@ -1,10 +1,20 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
-    cc::Build::new()
-        .file("rpi_ws281x/ws2811.c")
-        .file("rpi_ws281x/dma.c")
-        .file("rpi_ws281x/pcm.c")
-        .file("rpi_ws281x/pwm.c")
-        .file("rpi_ws281x/mailbox.c")
-        .file("rpi_ws281x/rpihw.c")
-        .compile("ws281x");
+    println!("cargo:rerun-if-env-changed=WS281X_LIB_DIR");
+    match env::var_os("WS281X_LIB_DIR").map(PathBuf::from) {
+        Some(lib_dir) => {
+            println!("cargo:rustc-link-search=native={}", lib_dir.display());
+            println!("cargo:rustc-link-lib=ws281x");
+        }
+        None => cc::Build::new()
+            .file("rpi_ws281x/ws2811.c")
+            .file("rpi_ws281x/dma.c")
+            .file("rpi_ws281x/pcm.c")
+            .file("rpi_ws281x/pwm.c")
+            .file("rpi_ws281x/mailbox.c")
+            .file("rpi_ws281x/rpihw.c")
+            .compile("ws281x"),
+    }
 }
